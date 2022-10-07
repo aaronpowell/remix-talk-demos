e7c87be3d16fe744584c37b527e85bd6d020ff60
Aaron Powell <me@aaron-powell.com>
2022-10-07 14:35:39 +1100

    better spinner

--- packages/03-csr/src/Loading.tsx
+++ packages/03-csr/src/Loading.tsx
@@ -1,7 +1,19 @@
+import "./Loading.css";
 export const Loading = () => {
   return (
-    <div className="loading">
-      <p>Loading...</p>
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
