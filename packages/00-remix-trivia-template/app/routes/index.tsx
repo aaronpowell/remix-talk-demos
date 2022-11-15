49a554614e77dc529ea68c6baf20f8253da758a4
Aaron Powell <me@aaron-powell.com>
2022-11-15 15:05:08 +1100

    spelling fix

--- packages/00-remix-trivia-template/app/routes/index.tsx
+++ packages/00-remix-trivia-template/app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div>
-      <h1>Remix triva app template</h1>
+      <h1>Remix trivia app template</h1>
       <p>This is the starting point for our Remix trivia app</p>
     </div>
   );
